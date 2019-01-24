d3f30ee601bd36eb920a6c247ef463feb2406fca
Unknown <creedc2002@gmail.com>
2019-01-24 11:32:05 -0500

    new

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Diff_ing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("sr-sample-input.txt");
+
+            //string strA = line.;
+            //string strB = ;
+
+            foreach (string line in lines)
+            {
+                int length = line.Length;
+                //Console.WriteLine(line.Length);
+
+                string[] words = line.Split(' ');
+                foreach (var word in words)
+                {
+                    //if (strA.Contains(word))
+                    Console.WriteLine($"{word}");
+                }
+            }
+        }
+    }
+}
